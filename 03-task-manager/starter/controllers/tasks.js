4fae3c6585bdc94bc758f1b291ecbf97fc376328
JarirTech <bjarir001@gmail.com>
2025-04-21 18:27:23 -0400

    answers for week6

--- 03-task-manager/starter/controllers/tasks.js
+++ 03-task-manager/starter/controllers/tasks.js
@@ -1,36 +1,62 @@
+const Task = require('../models/Task')
+const asyncWrapper = require('../middleware/async')
+const { createCustomError } = require('../errors/custom-error')
 
-const Task = require('../models/task')
+// get all tasks
+const getAllTasks = asyncWrapper(async (req, res) => {
+  const tasks = await Task.find({})
+  res.status(200).json({ tasks })
+})
 
-const getAllTasks = (req, res)=>{
-    res.send('get all tasks')
+// create a task
 
-}
+const createTask = asyncWrapper(async (req, res) => {
+  const task = await Task.create(req.body)
+  res.status(201).json({ task })
+})
 
-const createTask = async(req, res)=>{
-    
-    const task = await Task.create(req.body)
+// get a single task
 
-    res.status(201).json({task})
+const getTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOne({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
 
-}
+  res.status(200).json({ task })
+})
 
-const getTask = (req, res)=>{
+// delete a task
+const deleteTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
+  const task = await Task.findOneAndDelete({ _id: taskID })
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
+  res.status(200).json({ task })
+})
 
-    res.json({id:req.params.id})
+// update a task
+const updateTask = asyncWrapper(async (req, res, next) => {
+  const { id: taskID } = req.params
 
-}
-const updateTask = (req, res)=>{
+  const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+    new: true,
+    runValidators: true,
+  })
 
-    res.send('update a task')
+  if (!task) {
+    return next(createCustomError(`No task with id : ${taskID}`, 404))
+  }
 
-}
-
-const deleteTask = (req, res)=>{
-
-    res.send('delete a task')
-
-}
+  res.status(200).json({ task })
+})
 
 module.exports = {
-    getAllTasks, createTask, getTask, updateTask, deleteTask
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
 }
--- 03-task-manager/starter/routes/tasks.js
+++ 03-task-manager/starter/routes/tasks.js
@@ -1,15 +1,16 @@
-//route
-const express = require('express');
-const router = express.Router();
-const { getAllTasks, createTask, getTask, updateTask, deleteTask } = require('../controllers/tasks');
+const express = require('express')
+const router = express.Router()
 
+const {
+  getAllTasks,
+  createTask,
+  getTask,
+  updateTask,
+  deleteTask,
+  editTask,
+} = require('../controllers/tasks')
 
 router.route('/').get(getAllTasks).post(createTask)
 router.route('/:id').get(getTask).patch(updateTask).delete(deleteTask)
-
-
-
-
-
 
 module.exports = router
